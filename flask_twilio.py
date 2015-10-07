2e045a7fdc18231681109340c5b15c2f24b40ba3
Leo Singer <leo.singer@nasa.gov>
2015-10-07 06:47:00 -0400

    Use six to get urlparse functions

--- flask_twilio.py
+++ flask_twilio.py
@@ -4,7 +4,7 @@ __all__ = ('Response', 'Twilio')
 from base64 import urlsafe_b64encode
 from functools import wraps
 from os import urandom
-from urlparse import urlsplit, urlunsplit
+from six.moves.urllib.parse import urlsplit, urlunsplit
 from twilio.rest import TwilioRestClient
 from twilio.util import RequestValidator
 from twilio.twiml import Response as TwimlResponse
